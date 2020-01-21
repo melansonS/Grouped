7bab41be1fbea9eb8da5d941028a882f036c3f5d
melansonS <samuelmelanson@live.com>
2020-01-20 22:59:20 -0500

    first actual commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Test from "./Test";
+
+function App() {
+  return (
+    <div className="App">
+      <a href="#" className="icon">
+        <div>grouped</div>
+        <div className="pink_comma">.</div>
+      </a>
+      <h1>Groups</h1>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
